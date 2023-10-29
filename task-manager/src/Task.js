fe27f125eb75da5f7d92320424cf25571be216a7
Tarbiee <dorcusstaby@gmail.com>
2023-10-29 22:53:28 +0300

    Import styling

--- task-manager/src/Task.js
+++ task-manager/src/Task.js
@@ -1,4 +1,9 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './App.css';
+import {  faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
 
 function Task(){
 
